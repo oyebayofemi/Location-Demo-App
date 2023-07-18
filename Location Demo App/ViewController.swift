708b3d29b2c1cbdd11d271831b5ada4061124b0a
oyebayofemi <oyebayooluwafemi@gmail.com>
2023-07-18 16:42:40 -0400

    Initial Commit

---
+++ Location Demo App/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Location Demo App
+//
+//  Created by oyebayo oluwafemi on 2023-07-18.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
